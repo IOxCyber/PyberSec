f34aadf0bef4a08fdffab3b5556bbf23fea8e97c
CyberCuriosity <40174034+cyberqurious@users.noreply.github.com>
2023-01-06 13:58:57 +0000

    Updated Operator file

---
+++ DataTypes002.py
@@ -0,0 +1,17 @@
+# int & float
+print(type(6))
+print(5 + 4, (5 - 4))  # Add&Sub
+print(6 * 8)  # Multiply
+print(6 / 2)
+# Division -> Gives Quotient as in (Dividend ÷ Divisor = Quotient + Remainder)
+
+print(2**3)  # Power
+print(6 // 2)  # (Quotient) Round Off to closed Integer
+print(6 % 2)  # Modulo (Remainder)
+
+# math functions (Search on google for more)
+print(round(2.6))
+print(abs(2.6))  # No -ve number
+
+# Operater Precedence
+print((2+2)*2-4) # () > ** > *, / > +,-
