287fec91358ca26ef3e8250c468f06a5080029cb
CyberCuriosity <40174034+cyberqurious@users.noreply.github.com>
2023-02-02 10:55:23 +0000

    Try-Except-Else-Finally

--- ConceptCode002/Try-ExceptHandling014.py
+++ ConceptCode002/Try-ExceptHandling014.py
@@ -41,3 +41,15 @@ def sum(n1, n2):
 print(sum(1 + '2'))
 
 
+# Example 4:
+while True:
+  try:
+    age = int(input("Enter Age: "))
+    10 / age  # different type of Error
+  except (ValueError, ZeroDivisionError):  # 1st error handled
+    print("Try again, Age must be a number & greater than zero.")
+  else:
+    print('Thank you!')
+    break
+  finally: # run after every iteration executed.
+    print("Logs captured.")
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-# Example 4:
+# Example 5:
 while True:
   try:
     age = int(input("Enter Age: "))
-    10 / age  # different type of Error
-  except (ValueError, ZeroDivisionError):  # 1st error handled
-    print("Try again, Age must be a number & greater than zero.")
+    10 / age  # (Error Prone Code)
+  except ValueError:  # 1st error handled
+    print("Try again, Age must be a number.")
+    continue
+  except ZeroDivisionError:  # 2nd error handled
+    print("Try again, Age must be greater than Zero.")
+    break
   else:
     print('Thank you!')
-    break
   finally:
-    print("Logged captured.")
-  print("dff")
+    print('Finally, run everytime!')
+  print('The-End!')
+
+# Working of code > while > try > error? > catch it in except > go to while now.
+  
