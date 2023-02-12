f017c361c40793cf1621ead102855797f0676bcb
CyberCuriosity <40174034+cyberqurious@users.noreply.github.com>
2023-02-12 16:41:52 +0530

    Update hackinCode007.py

--- UseCasesEx003/hackinCode007.py
+++ UseCasesEx003/hackinCode007.py
@@ -4,7 +4,14 @@ x=string.ascii_lowercase
 print(x)
 
 # To print longest string of a list:
-# use inbuilt method max(iterable, key=len)
-print(max(list1, key=len))
-
+# Use inbuilt method max(iterable, key=len)
+# Example
+def longest_word(text):
+    try:
+        ls = list(text.split())
+        result = max(ls, key=len)
+        return result
+    except ValueError:
+        pass
+longest_word("Python is pure OOPs language.")
 
