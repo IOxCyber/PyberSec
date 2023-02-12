65f7fea5a199dd96b70045bd37ab1029ca65724e
CyberCuriosity <40174034+cyberqurious@users.noreply.github.com>
2023-02-12 23:20:51 +0530

    Update hackinCode007.py

--- UseCasesEx003/hackinCode007.py
+++ UseCasesEx003/hackinCode007.py
@@ -15,3 +15,19 @@ def longest_word(text):
         pass
 longest_word("Python is pure OOPs language.")
 
+
+# 
+# Example: sorting [https://docs.python.org/3/howto/sorting.html]
+# list.sort() - in-place sorting/returns None VS sorted(list) - returns a sorted list
+
+# Write a function named sort_by_mark that take as argument a list of students and returns a copy of it sorted by mark in descending order
+def sort_by_mark(my_class):
+  ls = sorted(my_class)
+  return ls[::-1]
+      
+# Write a function named sort_by_name that take as argument a list of students and returns a copy of it sorted by name in ascending order
+def sort_by_name(my_class):
+  ls = sorted(my_class, key=lambda my_class: my_class[1])     
+  return ls
+
+
